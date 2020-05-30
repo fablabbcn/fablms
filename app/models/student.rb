ecd1f91461703c0b3f64ca88dc45899012196be8
Viktor Smari <viktorsmari@gmail.com>
2020-05-30 17:00:44 +0200

    Change scope message

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,7 @@ class Student < ApplicationRecord
   belongs_to :course, optional: true
 
   validates :username, uniqueness: { scope: :organization, message: 'username taken' }
-  validates :email, uniqueness: { scope: :organization, message: 'username taken' }
+  validates :email, uniqueness: { scope: :organization, message: 'email taken' }
 
   def self.import_csv(file, organization, course)
     CSV.foreach(file.path, headers: true, col_sep: ';') do |row|
